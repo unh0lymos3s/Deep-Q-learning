7c6fbf37413a41e7c710fffccdf28204f9d818c8
Moosa <137150251+unh0lymos3s@users.noreply.github.com>
2025-01-07 23:19:56 +0500

    Add files via upload
    Main project file (doing as much as possible every day fr ong)

---
+++ DQN-doom.py
@@ -0,0 +1,42 @@
+import tensorflow as tf
+import numpy as np
+import vizdoom    
+from skimage import transform
+from collections import deque
+import matplotlib.pyplot as plt
+import warnings
+
+warnings.filterwarnings("ignore")
+
+episodes = None
+
+def create_environment ():
+    game = vizdoom.DoomGame()
+
+    game.load_config("basic.cfg")
+    game.set_doom_scenario_path("basic.wad")
+    game.init()
+
+    left = [1,0,0]
+    right = [0,1,0]
+    shoot = [0,0,1]     
+    possible_actions = [left, right, shoot]
+
+    return game, possible_actions
+
+def test_environment():
+    global episodes, actions
+    game = vizdoom.DoomGame()
+    game.load_config("../../scenarios/basic.cfg")
+    game.set_doom_scenario_path("../../scenarios/basic.wad")
+    game.init()
+    left = [1,0,0]
+    right = [0,1,0]
+    shoot = [0,0,1]
+    actions = [left,right,shoot]    
+    episodes = 10
+
+    for i in range(episodes):
+        game.new_episode()
+        # while not game.is_episode_finished():
+
